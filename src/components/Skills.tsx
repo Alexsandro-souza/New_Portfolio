6b7976eba4f050debea1e11547bdad64965f7bca
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-08-07 17:31:07 -0300

    feat: Adding toggle button to see more and see less in skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,16 +6,24 @@ import TypeScript from '../../public/TypeScript.svg';
 import Figma from '../../public/Figma.svg';
 import StyledComponents from '../../public/StyledComponents.svg';
 import ThreeTec from '../../public/ThreeTec.svg';
+import { useState } from 'react';
 
 const Skills = styled.section`
     margin: 5% auto 0 auto;
     max-width: 1000px;
     border-top: 3px solid #1B8E99;
     
+    
     @media(max-width: 980px){
         border-top: 0;
         
     }
+
+`;
+
+const Container = styled.div<{Visible: number}>`
+    height: ${props=>props.Visible === 0? `auto`:`450px`};
+    overflow: ${props=>props.Visible === 0? `visible`:`hidden`};
 
 `;
 
@@ -76,94 +84,113 @@ const List = styled.li`
     font-style: italic;
 `;
 
+const Span = styled.span`
+    cursor: pointer;
+    display: inline-block;
+    margin-top: 20px;
+    font-weight: bold;
+    font-style: italic;
+
+`
+
 
 const Skill : React.FC = ()=>{
+    const [visible, setVisible] = useState(true);
+    const [text, setText] = useState('Ver mais...');
 
-
+    const handleClick = ()=>{
+        setVisible(!visible)
+        !visible ? setText('Ver mais...'):setText('Ver menos...')
+    }
     return(
         <>
-        <Skills id='Skill'>
-            <TitleSingle>Habilidades</TitleSingle>
-            <WapperContainer>
+        <Skills  id='Skill'>
+            <Container Visible={visible? 1: 0}>
+                <TitleSingle>Habilidades</TitleSingle>
+                <WapperContainer>
+                    <Wrapper>
+                        <Title>HTML, Css e JavaScript</Title>
+                        <Text>São as principais tecnologias usadas na criação de páginas e aplicativos web. Cada uma delas desempenha um papel específico na construção e estilização das interfaces e no comportamento interativo. As duas primeiras são linguagem de marcação e estilo respectivamente, e o javascript é uma linguagem muito poderosa e alto nível, o que torna o aprendizado mais leve. Essas três tecnologias, HTML, CSS e JavaScript, trabalhando em conjunto, criam uma experiência interativa e envolvente para os usuários em páginas e aplicativos web modernos.</Text>
+                    </Wrapper>
+                    <WrapperContainerImg>
+                        <WrapperImg>
+                            <img style={{width: '60%', height:'80%'}} src={ThreeTec}/>
+                        </WrapperImg>
+                    </WrapperContainerImg>
+                </WapperContainer>    
+                <WapperContainer>
+                    <Wrapper>
+                        <Title>Sass e Less</Title>
+                        <Text>O Sass (Syntactically Awesome Style Sheets) e o Less (Leaner CSS) são pré-processadores de CSS que estendem a funcionalidade do CSS tradicional, trazendo diversas facilidades e vantagens para a estilização de páginas web. Quando conheci esses pré-processadores me familiarizei mais com o Less. Ambos têm o objetivo de tornar o processo de escrita de estilos mais eficiente e organizado.</Text>
+                        
+                    </Wrapper>
+                    <WrapperContainerImg>
+                        <WrapperImg>
+                            <img style={{width: '40%', height:'80%'}} src={Less}/>
+                            <img style={{width: '40%', height:'80%'}} src={Sass}/>
+                        </WrapperImg>
+                    </WrapperContainerImg>
+                    
+                </WapperContainer>
+            
+                <WapperContainer>
+                    <Wrapper>
+                        <Title>TypeScript</Title>
+                        <Text>TypeScript me permite escrever código mais seguro e confiável, aproveitando recursos como tipagem estática e inferência de tipos
+                        Com o conhecimento de JavaScript e TypeScript basta eu escolher qual a melhor linguagem para o projeto que irei fazer, sabendo que TypeScript basicamente JavaScript com tipagem estática, então é ótimo para projeto em equipe.</Text>
+                    </Wrapper>
+                    <WrapperContainerImg>
+                        <WrapperImg>
+                            <img style={{width: '40%', height:'80%'}} src={TypeScript}/>
+                        </WrapperImg>
+                    </WrapperContainerImg>
+                </WapperContainer>
+                <WapperContainer>
+                    <Wrapper>
+                        <Title>Styled Components</Title>
+                        <Text>O Styled Components é uma biblioteca popular que permite estilizar componentes React usando CSS-in-JS. Ele oferece uma maneira eficiente e flexível de criar estilos para componentes React, tornando o desenvolvimento de interfaces de usuário mais organizado e escalável. Além disso tem curva de aprendizado curta se o aprendiz já domina Css. O Styled Components trás muitos benefícios alguns são legibilidade e organização, componentes reutilizáveis, props nos estilos...</Text>
+                    </Wrapper>   
+                    <WrapperContainerImg>
+                        <WrapperImg>
+                            <img style={{width: '60%', height:'80%'}} src={StyledComponents}/>
+                        </WrapperImg>
+                    </WrapperContainerImg>
+                </WapperContainer>
+                <WapperContainer>
+                    <Wrapper>
+                        <Title>React</Title>
+                        <Text>React é significativo no desenvolvimento de aplicativos web modernos. Demorei um pouco para me familiarizar com alguns do seu atributos como Hooks, mas depois que aprendi meus projetos são feito muito mais rápidos do que utilizando apenas Html, Css e JavaScript, sem nenhum framework ou biblioteca. O React facilidades como componentização, virtual DOM, robusto, muito bem difundido.</Text>
+                    </Wrapper>
+                    <WrapperContainerImg>
+                        <WrapperImg>
+                            <img style={{width: '40%', height:'80%'}} src={React}/>
+                        </WrapperImg>
+                    </WrapperContainerImg>
+                </WapperContainer>
+                <WapperContainer>
+                    <Wrapper>
+                        <Title>Figma</Title>
+                        <Text>Como desenvolvedor, mesmo sem ser um designer profissional, possuo conhecimento em Figma. Essa habilidade facilita para uma solução de projeto mais elegante e precisa. A combinação de habilidades em desenvolvimento e noções básicas de design me torna mais versátil e apto a contribuir de forma mais completa para o sucesso dos projetos digitais. Estou comprometido em continuar aprendendo e aprimorando minhas habilidades para alcançar resultados excepcionais.</Text>
+                    </Wrapper>
+                    <WrapperContainerImg>
+                        <WrapperImg>
+                            <img style={{maxWidth: '200px', height:'80%'}} src={Figma}/>
+                        </WrapperImg>
+                    </WrapperContainerImg>
+                </WapperContainer>
+                    
                 <Wrapper>
-                    <Title>HTML, Css e JavaScript</Title>
-                    <Text>São as principais tecnologias usadas na criação de páginas e aplicativos web. Cada uma delas desempenha um papel específico na construção e estilização das interfaces e no comportamento interativo. As duas primeiras são linguagem de marcação e estilo respectivamente, e o javascript é uma linguagem muito poderosa e alto nível, o que torna o aprendizado mais leve. Essas três tecnologias, HTML, CSS e JavaScript, trabalhando em conjunto, criam uma experiência interativa e envolvente para os usuários em páginas e aplicativos web modernos.</Text>
+                    <Title>Estudando...</Title>
+                    <ul>
+                        <List>NodeJs</List>
+                        <List>MySql</List>
+                        <List>MongoDB</List>
+                    </ul>
                 </Wrapper>
-                <WrapperContainerImg>
-                    <WrapperImg>
-                        <img style={{width: '60%', height:'80%'}} src={ThreeTec}/>
-                    </WrapperImg>
-                </WrapperContainerImg>
-            </WapperContainer>    
-            <WapperContainer>
-                <Wrapper>
-                    <Title>Sass e Less</Title>
-                    <Text>O Sass (Syntactically Awesome Style Sheets) e o Less (Leaner CSS) são pré-processadores de CSS que estendem a funcionalidade do CSS tradicional, trazendo diversas facilidades e vantagens para a estilização de páginas web. Quando conheci esses pré-processadores me familiarizei mais com o Less. Ambos têm o objetivo de tornar o processo de escrita de estilos mais eficiente e organizado.</Text>
-                </Wrapper>
-                <WrapperContainerImg>
-                    <WrapperImg>
-                        <img style={{width: '40%', height:'80%'}} src={Less}/>
-                        <img style={{width: '40%', height:'80%'}} src={Sass}/>
-                    </WrapperImg>
-                </WrapperContainerImg>
-            </WapperContainer>
-            <WapperContainer>
-                <Wrapper>
-                    <Title>TypeScript</Title>
-                    <Text>TypeScript me permite escrever código mais seguro e confiável, aproveitando recursos como tipagem estática e inferência de tipos
-                    Com o conhecimento de JavaScript e TypeScript basta eu escolher qual a melhor linguagem para o projeto que irei fazer, sabendo que TypeScript basicamente JavaScript com tipagem estática, então é ótimo para projeto em equipe.</Text>
-                </Wrapper>
-                <WrapperContainerImg>
-                    <WrapperImg>
-                        <img style={{width: '40%', height:'80%'}} src={TypeScript}/>
-                    </WrapperImg>
-                </WrapperContainerImg>
-            </WapperContainer>
-            <WapperContainer>
-                <Wrapper>
-                    <Title>Styled Components</Title>
-                    <Text>O Styled Components é uma biblioteca popular que permite estilizar componentes React usando CSS-in-JS. Ele oferece uma maneira eficiente e flexível de criar estilos para componentes React, tornando o desenvolvimento de interfaces de usuário mais organizado e escalável. Além disso tem curva de aprendizado curta se o aprendiz já domina Css. O Styled Components trás muitos benefícios alguns são legibilidade e organização, componentes reutilizáveis, props nos estilos...</Text>
-                </Wrapper>   
-                <WrapperContainerImg>
-                    <WrapperImg>
-                        <img style={{width: '60%', height:'80%'}} src={StyledComponents}/>
-                    </WrapperImg>
-                </WrapperContainerImg>
-            </WapperContainer>
-            <WapperContainer>
-                <Wrapper>
-                    <Title>React</Title>
-                    <Text>React é significativo no desenvolvimento de aplicativos web modernos. Demorei um pouco para me familiarizar com alguns do seu atributos como Hooks, mas depois que aprendi meus projetos são feito muito mais rápidos do que utilizando apenas Html, Css e JavaScript, sem nenhum framework ou biblioteca. O React facilidades como componentização, virtual DOM, robusto, muito bem difundido.</Text>
-                </Wrapper>
-                <WrapperContainerImg>
-                    <WrapperImg>
-                        <img style={{width: '40%', height:'80%'}} src={React}/>
-                    </WrapperImg>
-                </WrapperContainerImg>
-            </WapperContainer>
-            <WapperContainer>
-                <Wrapper>
-                    <Title>Figma</Title>
-                    <Text>Como desenvolvedor, mesmo sem ser um designer profissional, possuo conhecimento em Figma. Essa habilidade facilita para uma solução de projeto mais elegante e precisa. A combinação de habilidades em desenvolvimento e noções básicas de design me torna mais versátil e apto a contribuir de forma mais completa para o sucesso dos projetos digitais. Estou comprometido em continuar aprendendo e aprimorando minhas habilidades para alcançar resultados excepcionais.</Text>
-                </Wrapper>
-                <WrapperContainerImg>
-                    <WrapperImg>
-                        <img style={{maxWidth: '200px', height:'80%'}} src={Figma}/>
-                    </WrapperImg>
-                </WrapperContainerImg>
-            </WapperContainer>
-                
-            <Wrapper>
-                <Title>Estudando...</Title>
-                <ul>
-                    <List>NodeJs</List>
-                    <List>MySql</List>
-                    <List>MongoDB</List>
-                </ul>
-            </Wrapper>
-
-
+            </Container>
+            <Span onClick={handleClick}>{text}</Span>          
         </Skills>
+      
         </>
     )
 }
