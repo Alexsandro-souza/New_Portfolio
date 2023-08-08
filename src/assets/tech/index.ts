10486a1868f474f696e1885bb2546574a5bbfbec
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-08-08 15:49:54 -0300

    feat: adding 3d elements to component skill

---
+++ src/assets/tech/index.ts
@@ -0,0 +1,22 @@
+
+import javascript from './javascript.png';
+import typescript from './typescript.png';
+import html from './html.png';
+import css from './css.png';
+import reactjs from './reactjs.png';
+import nodejs from './nodejs.png';
+import mongodb from './mongodb.png';
+import git from './git.png';
+import figma from './figma.png';
+
+export {
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+};
---
+++ src/components/Icons/balls/balls.tsx
@@ -0,0 +1,64 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../../Loader";
+
+interface BallProps {
+    imgUrl: string;
+  }
+
+const Ball: React.FC<BallProps>= (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
+  return (
+    <Float speed={5} rotationIntensity={1.1} floatIntensity={5}>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[51, 40, 11.0]} />
+      <mesh castShadow receiveShadow scale={1.5}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#1B8E99"
+          polygonOffset
+          polygonOffsetFactor={1}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
+};
+
+interface BallCanvas {
+    icon: string;
+  }
+
+const BallCanvas: React.FC<BallCanvas> = ({ icon}) => {
+  return (
+    <Canvas
+      frameloop="always"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
