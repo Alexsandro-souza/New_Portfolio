8d2ffa83b3bdc5e8cbcd7e5832e86e028b63db10
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-08-09 12:07:42 -0300

    refactor: Improve application responsiveness in Certificate

---
+++ src/assets/img/index.ts
@@ -0,0 +1,12 @@
+
+import Webmaster from './Webmaster.jpg';
+import ReactCurso from './ReactCurso.jpg';
+import JavaScriptCurso from './JavaScriptCurso.jpg';
+// import TypeScript from './TypeScript.jpg';
+
+export {
+  Webmaster,
+  ReactCurso,
+  JavaScriptCurso,
+//   TypeScript
+};
--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
-import danki from '../assets/img/Danki-Code.jpg';
+import danki from '../assets/img/Danki-Code.png';
+import {Webmaster, ReactCurso, JavaScriptCurso} from '../assets/img/index';
 
+
+interface InterfaceProps{
+    direction ?:number;
+    imgCourse : string;
+}
 
 const CertificateSection = styled.section`
     display: flex;
@@ -8,47 +14,81 @@ const CertificateSection = styled.section`
     max-width: 1000px;
     margin: 0 auto;
     height: auto;
+    
 `;
 
-const ContainerCertificate = styled.div<{direction?:number}>`
+const ContainerCertificate = styled.div<InterfaceProps>`
     position: relative;
     display: flex;
     flex-direction: ${props=>props.direction?'row-reverse':'row'};
     max-width: 100%;
     height: auto;
+    padding: 0 2%;
+
+
+    @media(max-width: 780px){
+        flex-direction: row !important;
+    }
 `;
 const WrapperCertificate = styled.div`
     position: relative;
     margin: 15px 0;
     width: 45%;
     height: 303px;
-    border: 2px solid #FFC300;
     border-radius: 2%;
+   
+
+    @media(max-width: 780px){
+        width: 85%;
+    }
     
 `;
+
+const WrapperImgCourse = styled.div<InterfaceProps>`
+    width: 100%;
+    height: 250px;
+    margin: 1% auto 0 auto;
+    background-image: url(${props => props.imgCourse});
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+`
 const VerticalLine = styled.div`
     position: absolute;
     height: 100%;
-    width: 4px;
+    width: 3px;
     left: 50%;
     transform: translateX(-50%);
     background-color: black;
+
+    @media (min-width: 210px) and (max-width: 780px){
+       left: 93%;
+    }
 `;
 
 const ImgCertificate = styled.div<{img?:string}>`
-    border: 3px solid #FFC300;
-    background-color: #FFC300;
+    border: 2px solid black;
     position: absolute;
     left: 50%;
     top: 40%;
     transform: translateX(-50%);
     height: 50px;
-    width: 50px;
+    width: 53px;
     border-radius: 50%;
     background-image: url(${props=>props.img});
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
+
+    @media(max-width: 780px){
+        height: 40px;
+        width: 43px;
+    }
+
+    @media(max-width: 330px){
+        height: 35px;
+        width: 38px;
+    }
     
 `;
 const TitleSingle = styled.h2`
@@ -67,18 +107,16 @@ const WrapperText = styled.div`
     bottom: 2px;
     padding: 2%;
     display: flex;
-    width: 40%;
+    width: 80%;
     border-top: 2px solid;
-    gap: 15px;
+    
 `;
 
 const TitleCourse = styled.h3`
     font-family: 'roboto',serif;
+    margin: 0 auto;
 `;
 
-const TexteCourse = styled.p`
-    font-family: 'roboto',serif;
-`;
 
 const Certificate : React.FC = ()=>{
 
@@ -89,9 +127,9 @@ const Certificate : React.FC = ()=>{
             <TitleSingle>Cursos e Certificados</TitleSingle>
             <ContainerCertificate>            
                 <WrapperCertificate>
+                    <WrapperImgCourse imgCourse={Webmaster}/>
                     <WrapperText>
-                        <TitleCourse>Curso exemlo</TitleCourse>
-                        <TexteCourse>exemplo</TexteCourse>
+                        <TitleCourse>Curso WebMaster Front-End completo</TitleCourse>
                     </WrapperText>
                 </WrapperCertificate>
                 <VerticalLine>
@@ -101,9 +139,9 @@ const Certificate : React.FC = ()=>{
 
             <ContainerCertificate direction={1}>
                 <WrapperCertificate>
+                    <WrapperImgCourse imgCourse={JavaScriptCurso}/>
                     <WrapperText>
-                        <TitleCourse>Curso exemlo</TitleCourse>
-                        <TexteCourse>exemplo</TexteCourse>
+                        <TitleCourse>JavaScript Completo</TitleCourse>    
                     </WrapperText>                   
                 </WrapperCertificate>
                 <VerticalLine>
@@ -111,6 +149,29 @@ const Certificate : React.FC = ()=>{
                 </VerticalLine>               
             </ContainerCertificate>
 
+            <ContainerCertificate>            
+                <WrapperCertificate>
+                    <WrapperImgCourse imgCourse={ReactCurso}/>
+                    <WrapperText>
+                        <TitleCourse>ReactJs</TitleCourse>
+                    </WrapperText>
+                </WrapperCertificate>
+                <VerticalLine>
+                        <ImgCertificate img={danki}/>
+                </VerticalLine>               
+            </ContainerCertificate>
+
+            <ContainerCertificate direction={1}>
+                <WrapperCertificate>
+                    <WrapperImgCourse imgCourse={ReactCurso}/>
+                    <WrapperText>
+                        <TitleCourse>Curso exemlo</TitleCourse>    
+                    </WrapperText>                   
+                </WrapperCertificate>
+                <VerticalLine>
+                    <ImgCertificate img={danki}/>
+                </VerticalLine>               
+            </ContainerCertificate>
         
 
            
