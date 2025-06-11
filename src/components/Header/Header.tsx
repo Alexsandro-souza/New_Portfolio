2ba5bcdf880a36b16fd6bc3633cc119cde811d25
Alexsandro Souza <115429488+Alexsandro-souza@users.noreply.github.com>
2025-06-11 12:52:47 -0300

    Change logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import logo from '../../../public/foto.png';
+import logo from '../../../public/logo.svg';
 import Navigate from './Nav';
 import SideBar from './SideBar';
 import styled from 'styled-components';
