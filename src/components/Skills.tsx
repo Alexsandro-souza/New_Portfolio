dba9474dcd808bd4e2686c60c336a1a9087fd1fd
Alexsandro Souza <115429488+Alexsandro-souza@users.noreply.github.com>
2025-06-11 17:24:34 -0300

    Change description in skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -187,7 +187,7 @@ const Skill : React.FC = ()=>{
                 <WapperContainer>
                     <Wrapper>
                         <Title>HTML, Css e JavaScript</Title>
-                        <Text>São as principais tecnologias usadas na criação de páginas e aplicativos web. Cada uma delas desempenha um papel específico na construção e estilização das interfaces e no comportamento interativo. As duas primeiras são linguagem de marcação e estilo respectivamente, e o javascript é uma linguagem muito poderosa e alto nível, o que torna o aprendizado mais leve. Essas três tecnologias, HTML, CSS e JavaScript, trabalhando em conjunto, criam uma experiência interativa e envolvente para os usuários em páginas e aplicativos web modernos.</Text>
+                        <Text>Essas três tecnologias, HTML, CSS e JavaScript, trabalhando em conjunto, criam uma experiência interativa e envolvente para os usuários em páginas e aplicativos web modernos. Os projetos apresentados nesse portfólio foram construídos utilizando essas stacks.</Text>
                     </Wrapper>
                         <WrapperContainerImg>                        
                             <WrapperImg>
@@ -210,33 +210,12 @@ const Skill : React.FC = ()=>{
                             </WrapperImgSingle>                        
                         </WrapperContainerImg>
                 </WapperContainer>
-                <WapperContainer>
-                    <Wrapper>
-                        <Title>Sass e Less</Title>
-                        <Text>O Sass (Syntactically Awesome Style Sheets) e o Less (Leaner CSS) são pré-processadores de CSS que estendem a funcionalidade do CSS tradicional, trazendo diversas facilidades e vantagens para a estilização de páginas web. Quando conheci esses pré-processadores me familiarizei mais com o Less. Ambos têm o objetivo de tornar o processo de escrita de estilos mais eficiente e organizado.</Text>
-                    </Wrapper>
-                    <WrapperContainerImg>
-                        <WrapperImg>
-                            <BallCanvas icon={Sass}/>
-                        </WrapperImg>
-                        <WrapperImgSingle>
-                            <Img src={Sass} alt='Imagem Sass'/>
-                        </WrapperImgSingle>
-                        <WrapperImg>
-                            <BallCanvas icon={Less}/>
-                        </WrapperImg>
-                        <WrapperImgSingle>
-                            <Img src={Less} alt='Imagem Less'/>
-                        </WrapperImgSingle>
-                    </WrapperContainerImg>
-                    
-                </WapperContainer>
+                
             
                 <WapperContainer>
                     <Wrapper>
                         <Title>TypeScript</Title>
-                        <Text>TypeScript me permite escrever código mais seguro e confiável, aproveitando recursos como tipagem estática e inferência de tipos
-                        Com o conhecimento de JavaScript e TypeScript basta eu escolher qual a melhor linguagem para o projeto que irei fazer, sabendo que TypeScript basicamente JavaScript com tipagem estática, então é ótimo para projeto em equipe.</Text>
+                        <Text>TypeScript é essencialmente JavaScript com tipagem estática, o que o torna uma escolha excelente para projetos que exigem robustez e escalabilidade. Com essa flexibilidade, posso desenvolver soluções de alta qualidade e confiáveis.</Text>
                     </Wrapper>
                     <WrapperContainerImg>
                         <WrapperImg>
@@ -250,7 +229,7 @@ const Skill : React.FC = ()=>{
                 <WapperContainer>
                     <Wrapper>
                         <Title>Styled Components</Title>
-                        <Text>O Styled Components é uma biblioteca popular que permite estilizar componentes React usando CSS-in-JS. Ele oferece uma maneira eficiente e flexível de criar estilos para componentes React, tornando o desenvolvimento de interfaces de usuário mais organizado e escalável. Além disso tem curva de aprendizado curta se o aprendiz já domina Css. O Styled Components trás muitos benefícios alguns são legibilidade e organização, componentes reutilizáveis, props nos estilos...</Text>
+                        <Text>Utilizei o Styled Components para criar os componentes desse portfólio, demonstrando minha habilidade em utilizar essa biblioteca para desenvolver interfaces de usuário modernas e escaláveis. Com essa experiência, posso criar soluções personalizadas e eficientes para meus projetos.</Text>
                     </Wrapper>   
                     <WrapperContainerImg>
                         <WrapperImg>
@@ -312,7 +291,7 @@ const Skill : React.FC = ()=>{
                 <WapperContainer>
                     <Wrapper>
                         <Title>Figma</Title>
-                        <Text>Como desenvolvedor, mesmo sem ser um designer profissional, possuo conhecimento em Figma. Essa habilidade facilita para uma solução de projeto mais elegante e precisa. A combinação de habilidades em desenvolvimento e noções básicas de design me torna mais versátil e apto a contribuir de forma mais completa para o sucesso dos projetos digitais. Estou comprometido em continuar aprendendo e aprimorando minhas habilidades para alcançar resultados excepcionais.</Text>
+                        <Text>Utilizo o Figma para acompanhar os designs que devo implementar, o que me permite ter uma visão clara e detalhada do que precisa ser desenvolvido.</Text>
                     </Wrapper>
                     <WrapperContainerImg>
                         <WrapperImg>
@@ -326,7 +305,7 @@ const Skill : React.FC = ()=>{
                 <WapperContainer>
                     <Wrapper>
                         <Title>Git e GitHub</Title>
-                        <Text>Como desenvolvedor, mesmo sem ser um designer profissional, possuo conhecimento em Figma. Essa habilidade facilita para uma solução de projeto mais elegante e precisa. A combinação de habilidades em desenvolvimento e noções básicas de design me torna mais versátil e apto a contribuir de forma mais completa para o sucesso dos projetos digitais. Estou comprometido em continuar aprendendo e aprimorando minhas habilidades para alcançar resultados excepcionais.</Text>
+                        <Text>Utilizo Git e GitHub para versionalizar todos os meus projetos, o que me permite ter controle total sobre as alterações realizadas no código. Com essa abordagem, posso garantir a segurança e a integridade dos meus projetos, mesmo durante a implementação de novas funcionalidades complexas.</Text>
                     </Wrapper>
                     <WrapperContainerImg>
                         <WrapperImg>
@@ -355,12 +334,6 @@ const Skill : React.FC = ()=>{
                                 MySql
                                <BallCanvas icon={MySql}/>
                             </WrapperImgstudying>                            
-                        </List>
-                        <List>
-                            <WrapperImgstudying style={{width:'110px', height:'80px', gap:'10px'}}>
-                                MongoDB
-                               <BallCanvas icon={mongodb}/>
-                           </WrapperImgstudying>
                         </List>
                     </ul>
                 </Wrapper>
